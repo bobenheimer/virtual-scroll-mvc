dc94163490a621744f161425f49bb4ca246428ae
Bob Toney <bobctoney@gmail.com>
2015-01-17 15:32:51 -0800

    finished up React

--- app/react/js/app.js
+++ app/react/js/app.js
@@ -1,206 +1,220 @@
-//http://facebook.github.io/react/blog/2013/11/05/thinking-in-react.html
-var App = React.createClass({displayName: "App",
-  getInitialState: function() {
-    return {
-      sortColumn: "name",
-      sortDirection: 1,
-      items: Common.list.get(Common.DEFAULT_LIST_SIZE)
-    };
-  },
+"use strict";
+(function() {
+  var $window = $(window);
+  var WAIT_TIME = 24;
 
-  setSort: function(name) {
-    var items = this.state.items.slice();
-    var sortColumn = this.state.sortColumn;
-    var sortDirection = this.state.sortDirection;
-
-    items.sort(function(a, b) {
-      return a[sortColumn] > b[sortColumn] ? sortDirection : -sortDirection;
-    });
-
-    this.setState({
-      sortColumn: name,
-      sortDirection: sortDirection * -1,
-      items: items
-    });
-  },
-  changeCount: function(count) {
-    this.setState({ items: Common.list.get(count) });
-  },
-  render: function() {
-    return (
-      React.createElement("div", null, 
-        React.createElement(MainHeader, {
-          changeCount: this.changeCount, 
-          currentCount: this.state.items.length}
-        ), 
-        React.createElement("div", {className: "main-content"}, 
-          React.createElement(ItemHeader, {
-            sortColumn: this.state.sortColumn, 
-            sortDirection: this.state.sortDirection, 
-            setSort: this.setSort}
-          ), 
-          React.createElement(ItemList, {
-            items: this.state.items}
-          )
-        )
-      )
-    )
-  }
-});
-
-var ItemList = React.createClass({displayName: "ItemList",
-  render: function() {
-    return (
-      React.createElement("div", {className: "item-list"}, 
-      
-        this.props.items.map(function(item, index) {
-          return (
-            React.createElement("div", {className: "item", key: item.name}, 
-              React.createElement("div", {className: "index"}, index+1), 
-              React.createElement("div", {className: "name"}, item.name), 
-              React.createElement("div", {className: "size"}, item.size), 
-              React.createElement("div", {className: "date"}, Common.formatDate(item.date))
-            )
-          )
-        })
-      
-      )
-    )
-  }
-});
-
-var ItemHeader = React.createClass({displayName: "ItemHeader",
-  render: function() {
-    var self = this;
-    var cx = React.addons.classSet;
-
-    var classesFor = function(column) {
-      var data = {
-        active: self.props.sortColumn === column,
-        up: self.props.sortDirection === 1,
-        down: self.props.sortDirection === -1
+  //http://facebook.github.io/react/blog/2013/11/05/thinking-in-react.html
+  var App = React.createClass({displayName: "App",
+    getInitialState: function() {
+      return {
+        sortColumn: "name",
+        sortDirection: 1,
+        items: Common.list.get(Common.DEFAULT_LIST_SIZE)
       };
-      data[column] = true;
-      return cx(data);
-    };
+    },
+    sortItems: function(items) {
+      items = items.slice(); //shallow copy
+      var sortColumn = this.state.sortColumn;
+      var sortDirection = this.state.sortDirection;
 
-    return (
-      React.createElement("div", {className: "item-header"}, 
-        React.createElement("div", {className: "index"}, 
-          React.createElement("span", null, "#")
-        ), 
-        React.createElement("div", {
-          className: classesFor("name"), onClick: self.props.setSort.bind(null, "name")}, 
-          React.createElement("span", null, "Name "), 
-          React.createElement("span", {className: "glyphicon"})
-        ), 
-        React.createElement("div", {className: classesFor("size"), onClick: self.props.setSort.bind(null, "size")}, 
-          React.createElement("span", null, "Size "), 
-          React.createElement("span", {className: "glyphicon"})
-        ), 
-        React.createElement("div", {className: classesFor("date"), onClick: self.props.setSort.bind(null, "date")}, 
-          React.createElement("span", null, "Date "), 
-          React.createElement("span", {className: "glyphicon"})
-        )
-      )
-    )
-  }
-});
+      items.sort(function(a, b) {
+        return a[sortColumn] > b[sortColumn] ? sortDirection : -sortDirection;
+      });
 
-var MainHeader = React.createClass({displayName: "MainHeader",
-  onChange: function(event) {
-    this.props.changeCount(event.target.value);
-  },
-  render: function() {
-    return (
-      React.createElement("div", {className: "header"}, 
-        React.createElement("div", {className: "header-left"}, 
-
-          React.createElement("h4", {className: "virtual-scroll"}, 
-            React.createElement("a", {href: "../"}, 
-              React.createElement("span", {className: "virtual-scroll-text"}, "Virtual Scroll Mvc"), 
-              React.createElement("span", {className: "glyphicon glyphicon-home"})
-            )
+      return items;
+    },
+    setSort: function(name) {
+      this.setState({
+        sortColumn: name,
+        sortDirection: this.state.sortDirection * -1
+      });
+    },
+    changeCount: function(count) {
+      var sorted = this.sortItems(Common.list.get(count));
+      this.setState({ items: sorted });
+    },
+    render: function() {
+      var sorted = this.sortItems(this.state.items);
+      return (
+        React.createElement("div", null, 
+          React.createElement(MainHeader, {
+            changeCount: this.changeCount, 
+            currentCount: this.state.items.length}
           ), 
-
-          React.createElement("div", {className: "modify-count"}, 
-            React.createElement("div", {className: "label"}, "# Items"), 
-            React.createElement("div", {className: "input-wrap"}, 
-              React.createElement("input", {type: "number", value: this.props.currentCount, onChange: this.onChange})
-            )
+          React.createElement("div", {className: "main-content"}, 
+            React.createElement(ItemHeader, {
+              sortColumn: this.state.sortColumn, 
+              sortDirection: this.state.sortDirection, 
+              setSort: this.setSort}
+            ), 
+            React.createElement(ItemList, {items: sorted})
           )
-
-        ), 
-
-        React.createElement("div", {className: "header-right"}, 
-          React.createElement("a", {target: "_blank", className: "framework", href: "http://facebook.github.io/react/"}, "ReactJS")
         )
       )
-    )
-  }
-});
+    }
+  });
 
-React.render(
-  React.createElement(App, null),
-  document.getElementById("foo")
-);
+  var ItemList = React.createClass({displayName: "ItemList",
+    getInitialState: function() {
+      return { visibleItems: [] };
+    },
+    componentDidMount: function() {
+      var self = this;
+      var $element = $(self.getDOMNode());
 
-var index = 0;
-var Loop = React.createClass({displayName: "Loop",
-  getInitialState: function() {
-    return {
-      items: [
-        {
-          key: index++,
-          value: "test1"
-        }
-      ]
-    };
-  },
-  addItems: function() {
-    this.setState({
-      items: this.state.items.concat({
-        key: index++,
-        value: "test" + index
-      })
-    });
-  },
-  render: function() {
-    return (
-      React.createElement("div", null, 
-        React.createElement("div", {onClick: this.addItems}, "add"), 
+      var calcFunction = function() {
+        self.calculateVisibleItems($element, self.props.items);
+      };
+
+      self.throttledScrollHandler = Common.misc.throttledFunction(calcFunction, WAIT_TIME);
+      self.resizeHandler = calcFunction;
+
+      //call the function right at the start
+      calcFunction();
+
+      $window.on("scroll", self.throttledScrollHandler);
+      $window.on("resize", self.resizeHandler);
+    },
+    componentWillReceiveProps: function(nextProps) {
+      if (!nextProps.items) { return; }
+
+      var $element = $(this.getDOMNode());
+      this.calculateVisibleItems($element, nextProps.items);
+
+      //scroll to the top when the items count changes
+      if (nextProps.items.length !== this.props.items.length) {
+        $window.scrollTop(0);
+      }
+    },
+    calculateVisibleItems: function($element, items) {
+      var scrollTop = $window.scrollTop();
+      var offset = $element.offset().top;
+
+      //the pixels relative to the outer listing wrapper
+      var startPixel = scrollTop - offset;
+      var endPixel = startPixel + $window.height();
+
+      if (startPixel < 0) {
+        startPixel = 0;
+      }
+
+      //start and end row of the items to show
+      var startRow = Math.floor(startPixel / Common.ROW_HEIGHT);
+      var endRow = Math.ceil(endPixel / Common.ROW_HEIGHT);
+
+      //extra padding at the top that represents the unrendered top elements
+      var paddingTop = Common.ROW_HEIGHT * startRow;
+
+      //extra padding at the bottom
+      var unrenderedBottomElements = items.slice(endRow);
+      var paddingBottom = Common.ROW_HEIGHT * unrenderedBottomElements.length;
+
+      var newVisibleItems = items.slice(startRow, endRow);
+
+      $element.css({
+        "padding-top": paddingTop + "px",
+        "padding-bottom": paddingBottom + "px"
+      });
+
+      this.setState({
+        visibleItems: newVisibleItems,
+        firstRenderedRow: startRow
+      });
+    },
+    render: function() {
+      var self = this;
+      return (
+        React.createElement("div", {className: "item-list"}, 
         
-          this.state.items.map(function(item, i){
-            return React.createElement("div", {key: item.key}, item.value)
+          self.state.visibleItems.map(function(item, index) {
+            return (
+              React.createElement("div", {className: "item", key: item.name}, 
+                React.createElement("div", {className: "index"}, self.state.firstRenderedRow + index), 
+                React.createElement("div", {className: "name"}, item.name), 
+                React.createElement("div", {className: "size"}, item.size), 
+                React.createElement("div", {className: "date"}, Common.formatDate(item.date))
+              )
+            )
           })
         
+        )
       )
-    );
-  }
-});
+    },
+    componentWillUnmount: function() {
+      $window.on("scroll", this.throttledScrollHandler);
+      $window.on("resize", this.resizeHandler);
+    }
+  });
 
-
-var Component = React.createClass({displayName: "Component",
-  render: function() {
-    return (
-      React.createElement("div", {style: this.props.style}, 
-        React.createElement("div", null, [this.props.foo,this.props.bar])
+  var ItemHeader = React.createClass({displayName: "ItemHeader",
+    classesFor: function(column) {
+      var data = {
+        active: this.props.sortColumn === column,
+        up: this.props.sortDirection === 1,
+        down: this.props.sortDirection === -1
+      };
+      data[column] = true;
+      return React.addons.classSet(data);
+    },
+    render: function() {
+      return (
+        React.createElement("div", {className: "item-header"}, 
+          React.createElement("div", {className: "index"}, 
+            React.createElement("span", null, "#")
+          ), 
+          React.createElement("div", {
+            className: this.classesFor("name"), onClick: this.props.setSort.bind(null, "name")}, 
+            React.createElement("span", null, "Name "), 
+            React.createElement("span", {className: "glyphicon"})
+          ), 
+          React.createElement("div", {className: this.classesFor("size"), onClick: this.props.setSort.bind(null, "size")}, 
+            React.createElement("span", null, "Size "), 
+            React.createElement("span", {className: "glyphicon"})
+          ), 
+          React.createElement("div", {className: this.classesFor("date"), onClick: this.props.setSort.bind(null, "date")}, 
+            React.createElement("span", null, "Date "), 
+            React.createElement("span", {className: "glyphicon"})
+          )
+        )
       )
-    )
-  }
-});
+    }
+  });
 
-var poops = {
-  foo: 'b',
-  bar: 'p',
-  style: {
-    height: "100px"
-  }
-};
+  var MainHeader = React.createClass({displayName: "MainHeader",
+    onChange: function(event) {
+      this.props.changeCount(event.target.value);
+    },
+    render: function() {
+      return (
+        React.createElement("div", {className: "header"}, 
+          React.createElement("div", {className: "header-left"}, 
 
-var component = React.createElement(Component, React.__spread({},  poops));
+            React.createElement("h4", {className: "virtual-scroll"}, 
+              React.createElement("a", {href: "../"}, 
+                React.createElement("span", {className: "virtual-scroll-text"}, "Virtual Scroll Mvc"), 
+                React.createElement("span", {className: "glyphicon glyphicon-home"})
+              )
+            ), 
 
-//React.render(
-//  component,
-//  document.getElementById('example')
-//);
+            React.createElement("div", {className: "modify-count"}, 
+              React.createElement("div", {className: "label"}, "# Items"), 
+              React.createElement("div", {className: "input-wrap"}, 
+                React.createElement("input", {type: "number", value: this.props.currentCount, onChange: this.onChange})
+              )
+            )
+
+          ), 
+
+          React.createElement("div", {className: "header-right"}, 
+            React.createElement("a", {target: "_blank", className: "framework", href: "http://facebook.github.io/react/"}, "ReactJS")
+          )
+        )
+      )
+    }
+  });
+
+  React.render(
+    React.createElement(App, null),
+    document.getElementById("app")
+  );
+})();
--- app/react/jsx/app.jsx
+++ app/react/jsx/app.jsx
@@ -1,206 +1,220 @@
-//http://facebook.github.io/react/blog/2013/11/05/thinking-in-react.html
-var App = React.createClass({
-  getInitialState: function() {
-    return {
-      sortColumn: "name",
-      sortDirection: 1,
-      items: Common.list.get(Common.DEFAULT_LIST_SIZE)
-    };
-  },
+"use strict";
+(function() {
+  var $window = $(window);
+  var WAIT_TIME = 24;
 
-  setSort: function(name) {
-    var items = this.state.items.slice();
-    var sortColumn = this.state.sortColumn;
-    var sortDirection = this.state.sortDirection;
-
-    items.sort(function(a, b) {
-      return a[sortColumn] > b[sortColumn] ? sortDirection : -sortDirection;
-    });
-
-    this.setState({
-      sortColumn: name,
-      sortDirection: sortDirection * -1,
-      items: items
-    });
-  },
-  changeCount: function(count) {
-    this.setState({ items: Common.list.get(count) });
-  },
-  render: function() {
-    return (
-      <div>
-        <MainHeader
-          changeCount={this.changeCount}
-          currentCount={this.state.items.length}
-        />
-        <div className="main-content">
-          <ItemHeader
-            sortColumn={this.state.sortColumn}
-            sortDirection={this.state.sortDirection}
-            setSort={this.setSort}
-          />
-          <ItemList
-            items={this.state.items}
-          />
-        </div>
-      </div>
-    )
-  }
-});
-
-var ItemList = React.createClass({
-  render: function() {
-    return (
-      <div className="item-list">
-      {
-        this.props.items.map(function(item, index) {
-          return (
-            <div className="item" key={item.name}>
-              <div className="index">{index+1}</div>
-              <div className="name">{item.name}</div>
-              <div className="size">{item.size}</div>
-              <div className="date">{Common.formatDate(item.date)}</div>
-            </div>
-          )
-        })
-      }
-      </div>
-    )
-  }
-});
-
-var ItemHeader = React.createClass({
-  render: function() {
-    var self = this;
-    var cx = React.addons.classSet;
-
-    var classesFor = function(column) {
-      var data = {
-        active: self.props.sortColumn === column,
-        up: self.props.sortDirection === 1,
-        down: self.props.sortDirection === -1
+  //http://facebook.github.io/react/blog/2013/11/05/thinking-in-react.html
+  var App = React.createClass({
+    getInitialState: function() {
+      return {
+        sortColumn: "name",
+        sortDirection: 1,
+        items: Common.list.get(Common.DEFAULT_LIST_SIZE)
       };
-      data[column] = true;
-      return cx(data);
-    };
+    },
+    sortItems: function(items) {
+      items = items.slice(); //shallow copy
+      var sortColumn = this.state.sortColumn;
+      var sortDirection = this.state.sortDirection;
 
-    return (
-      <div className="item-header">
-        <div className="index">
-          <span>{"#"}</span>
-        </div>
-        <div
-          className={classesFor("name")} onClick={self.props.setSort.bind(null, "name")}>
-          <span>Name </span>
-          <span className="glyphicon"></span>
-        </div>
-        <div className={classesFor("size")} onClick={self.props.setSort.bind(null, "size")}>
-          <span>Size </span>
-          <span className="glyphicon"></span>
-        </div>
-        <div className={classesFor("date")} onClick={self.props.setSort.bind(null, "date")}>
-          <span>Date </span>
-          <span className="glyphicon"></span>
-        </div>
-      </div>
-    )
-  }
-});
+      items.sort(function(a, b) {
+        return a[sortColumn] > b[sortColumn] ? sortDirection : -sortDirection;
+      });
 
-var MainHeader = React.createClass({
-  onChange: function(event) {
-    this.props.changeCount(event.target.value);
-  },
-  render: function() {
-    return (
-      <div className="header">
-        <div className="header-left">
-
-          <h4 className="virtual-scroll">
-            <a href="../">
-              <span className="virtual-scroll-text">Virtual Scroll Mvc</span>
-              <span className="glyphicon glyphicon-home"></span>
-            </a>
-          </h4>
-
-          <div className="modify-count">
-            <div className="label">{"# Items"}</div>
-            <div className="input-wrap">
-              <input type="number" value={this.props.currentCount} onChange={this.onChange} />
-            </div>
+      return items;
+    },
+    setSort: function(name) {
+      this.setState({
+        sortColumn: name,
+        sortDirection: this.state.sortDirection * -1
+      });
+    },
+    changeCount: function(count) {
+      var sorted = this.sortItems(Common.list.get(count));
+      this.setState({ items: sorted });
+    },
+    render: function() {
+      var sorted = this.sortItems(this.state.items);
+      return (
+        <div>
+          <MainHeader
+            changeCount={this.changeCount}
+            currentCount={this.state.items.length}
+          />
+          <div className="main-content">
+            <ItemHeader
+              sortColumn={this.state.sortColumn}
+              sortDirection={this.state.sortDirection}
+              setSort={this.setSort}
+            />
+            <ItemList items={sorted} />
           </div>
-
         </div>
+      )
+    }
+  });
 
-        <div className="header-right">
-          <a target="_blank" className="framework" href="http://facebook.github.io/react/">ReactJS</a>
-        </div>
-      </div>
-    )
-  }
-});
+  var ItemList = React.createClass({
+    getInitialState: function() {
+      return { visibleItems: [] };
+    },
+    componentDidMount: function() {
+      var self = this;
+      var $element = $(self.getDOMNode());
 
-React.render(
-  <App />,
-  document.getElementById("foo")
-);
+      var calcFunction = function() {
+        self.calculateVisibleItems($element, self.props.items);
+      };
 
-var index = 0;
-var Loop = React.createClass({
-  getInitialState: function() {
-    return {
-      items: [
+      self.throttledScrollHandler = Common.misc.throttledFunction(calcFunction, WAIT_TIME);
+      self.resizeHandler = calcFunction;
+
+      //call the function right at the start
+      calcFunction();
+
+      $window.on("scroll", self.throttledScrollHandler);
+      $window.on("resize", self.resizeHandler);
+    },
+    componentWillReceiveProps: function(nextProps) {
+      if (!nextProps.items) { return; }
+
+      var $element = $(this.getDOMNode());
+      this.calculateVisibleItems($element, nextProps.items);
+
+      //scroll to the top when the items count changes
+      if (nextProps.items.length !== this.props.items.length) {
+        $window.scrollTop(0);
+      }
+    },
+    calculateVisibleItems: function($element, items) {
+      var scrollTop = $window.scrollTop();
+      var offset = $element.offset().top;
+
+      //the pixels relative to the outer listing wrapper
+      var startPixel = scrollTop - offset;
+      var endPixel = startPixel + $window.height();
+
+      if (startPixel < 0) {
+        startPixel = 0;
+      }
+
+      //start and end row of the items to show
+      var startRow = Math.floor(startPixel / Common.ROW_HEIGHT);
+      var endRow = Math.ceil(endPixel / Common.ROW_HEIGHT);
+
+      //extra padding at the top that represents the unrendered top elements
+      var paddingTop = Common.ROW_HEIGHT * startRow;
+
+      //extra padding at the bottom
+      var unrenderedBottomElements = items.slice(endRow);
+      var paddingBottom = Common.ROW_HEIGHT * unrenderedBottomElements.length;
+
+      var newVisibleItems = items.slice(startRow, endRow);
+
+      $element.css({
+        "padding-top": paddingTop + "px",
+        "padding-bottom": paddingBottom + "px"
+      });
+
+      this.setState({
+        visibleItems: newVisibleItems,
+        firstRenderedRow: startRow
+      });
+    },
+    render: function() {
+      var self = this;
+      return (
+        <div className="item-list">
         {
-          key: index++,
-          value: "test1"
-        }
-      ]
-    };
-  },
-  addItems: function() {
-    this.setState({
-      items: this.state.items.concat({
-        key: index++,
-        value: "test" + index
-      })
-    });
-  },
-  render: function() {
-    return (
-      <div>
-        <div onClick={this.addItems}>add</div>
-        {
-          this.state.items.map(function(item, i){
-            return <div key={item.key}>{item.value}</div>
+          self.state.visibleItems.map(function(item, index) {
+            return (
+              <div className="item" key={item.name}>
+                <div className="index">{self.state.firstRenderedRow + index}</div>
+                <div className="name">{item.name}</div>
+                <div className="size">{item.size}</div>
+                <div className="date">{Common.formatDate(item.date)}</div>
+              </div>
+            )
           })
         }
-      </div>
-    );
-  }
-});
+        </div>
+      )
+    },
+    componentWillUnmount: function() {
+      $window.on("scroll", this.throttledScrollHandler);
+      $window.on("resize", this.resizeHandler);
+    }
+  });
 
+  var ItemHeader = React.createClass({
+    classesFor: function(column) {
+      var data = {
+        active: this.props.sortColumn === column,
+        up: this.props.sortDirection === 1,
+        down: this.props.sortDirection === -1
+      };
+      data[column] = true;
+      return React.addons.classSet(data);
+    },
+    render: function() {
+      return (
+        <div className="item-header">
+          <div className="index">
+            <span>{"#"}</span>
+          </div>
+          <div
+            className={this.classesFor("name")} onClick={this.props.setSort.bind(null, "name")}>
+            <span>Name </span>
+            <span className="glyphicon"></span>
+          </div>
+          <div className={this.classesFor("size")} onClick={this.props.setSort.bind(null, "size")}>
+            <span>Size </span>
+            <span className="glyphicon"></span>
+          </div>
+          <div className={this.classesFor("date")} onClick={this.props.setSort.bind(null, "date")}>
+            <span>Date </span>
+            <span className="glyphicon"></span>
+          </div>
+        </div>
+      )
+    }
+  });
 
-var Component = React.createClass({
-  render: function() {
-    return (
-      <div style={this.props.style}>
-        <div>{[this.props.foo,this.props.bar]}</div>
-      </div>
-    )
-  }
-});
+  var MainHeader = React.createClass({
+    onChange: function(event) {
+      this.props.changeCount(event.target.value);
+    },
+    render: function() {
+      return (
+        <div className="header">
+          <div className="header-left">
 
-var poops = {
-  foo: 'b',
-  bar: 'p',
-  style: {
-    height: "100px"
-  }
-};
+            <h4 className="virtual-scroll">
+              <a href="../">
+                <span className="virtual-scroll-text">Virtual Scroll Mvc</span>
+                <span className="glyphicon glyphicon-home"></span>
+              </a>
+            </h4>
 
-var component = <Component {...poops}/>;
+            <div className="modify-count">
+              <div className="label">{"# Items"}</div>
+              <div className="input-wrap">
+                <input type="number" value={this.props.currentCount} onChange={this.onChange} />
+              </div>
+            </div>
 
-//React.render(
-//  component,
-//  document.getElementById('example')
-//);
+          </div>
+
+          <div className="header-right">
+            <a target="_blank" className="framework" href="http://facebook.github.io/react/">ReactJS</a>
+          </div>
+        </div>
+      )
+    }
+  });
+
+  React.render(
+    <App />,
+    document.getElementById("app")
+  );
+})();
